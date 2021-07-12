914e8e3724fc3ae9296b905ea5da9692523669a2
JohnHeo04 <hjo199456@gmail.com>
2021-07-12 21:35:13 +0900

    Initial Commit

---
+++ Scene-Trans01/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  Scene-Trans01
+//
+//  Created by John Hur on 2021/07/12.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+
+    @IBAction func back(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
+    }
+    
+    
+}
