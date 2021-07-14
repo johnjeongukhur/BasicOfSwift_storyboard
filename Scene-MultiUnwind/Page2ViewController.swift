2a64f94278d8e4d0e4200af0c7bab7e9cc44dc5f
JohnHeo04 <hjo199456@gmail.com>
2021-07-14 15:18:26 +0900

    Initial Commit

---
+++ Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by John Hur on 2021/07/14.
+//
+
+import UIKit
+
+class Page2ViewController: UIViewController {
+    
+    @IBAction func gotoPage2(_ segue: UIStoryboardSegue) {
+        
+        
+    }
+    
+    
+}
---
+++ Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by John Hur on 2021/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func gotoPage1(_ sender: UIStoryboardSegue) {
+        
+        
+    }
+
+
+}
+
