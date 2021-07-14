83fd8ce3465d819976a937b26308e1691bdb9f2b
JohnHeo04 <hjo199456@gmail.com>
2021-07-14 12:47:54 +0900

    Initial Commit

---
+++ Scene-ActionSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scene-ActionSegue
+//
+//  Created by John Hur on 2021/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
