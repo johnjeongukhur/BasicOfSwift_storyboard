7c680c6538613a7fcc28ca8e2dcbdb9851769dc7
JohnHeo04 <hjo199456@gmail.com>
2021-07-16 17:51:20 +0900

    SubmitValue 동기방식으로 값을 저장하는 방법 익힘

--- SubmitValue/ResultViewController.swift
+++ SubmitValue/ResultViewController.swift
@@ -22,6 +22,16 @@ class ResultViewController: UIViewController {
     // 3 Interval 값을 받을 변수
     var paramInterval: Double = 0
     
+    // 화면이 메모리에 로드되고 난 시점에서 viewDidLoad() 메소드 호출
+    // 이를 '오버라이딩'이라고 부름
+    override func viewDidLoad() {
+        self.resultEmail.text = paramEmail
+        self.resultUpdate.text = (self.paramUpdate == true ? "자동갱신" : "자동갱신안함")
+        self.resultInterval.text = "\(Int(paramInterval))분 마다 갱신"
+    }
     
+    @IBAction func onBack(_ sender: Any) {
+        self.presentingViewController?.dismiss(animated: true)
+    }
     
 }
--- SubmitValue/ViewController.swift
+++ SubmitValue/ViewController.swift
@@ -42,5 +42,44 @@ class ViewController: UIViewController {
         self.intervalText.text = "\(value)분 마다"
     }
     
+    @IBAction func onSubmit(_ sender: Any) {
+        // VC2의 인스턴스 생성
+        guard let rvc = self.storyboard?.instantiateViewController(withIdentifier: "RVC") as? ResultViewController else { return }
+        
+        // 값을 전달하는 과정을 여기에 작성
+        // 값 전달
+        rvc.paramEmail = self.email.text! // 이메일
+        rvc.paramUpdate = self.isUpdate.isOn // 자동갱신 여부
+        rvc.paramInterval = self.interval.value // 갱신주기
+        
+        // 화면 이동
+        // 아래는 present, dismiss를 이용한 화면전환
+        self.present(rvc, animated: true)
+        // navigation을 이용한 화면전환
+        self.navigationController?.pushViewController(rvc, animated: true)
+    }
+    
+    // 세그웨이를 이용한 화면전환
+    @IBAction func onPerformSegue(_ sender: Any) {
+        self.performSegue(withIdentifier: "ManualSubmit", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // 목적지 뷰 컨트롤러 인스턴스 읽어오기
+        let dest = segue.destination
+        
+        guard let rvc = dest as? ResultViewController else {
+            return
+        }
+        // 값 전달
+        rvc.paramEmail = self.email.text!
+        rvc.paramUpdate = self.isUpdate.isOn
+        rvc.paramInterval = self.interval.value
+        
+    }
+    
+    
+    
 }
 
