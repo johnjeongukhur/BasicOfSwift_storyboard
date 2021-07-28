7aff2ce15a4ea176e26f59154a3f12ea5ca24e4d
JohnHeo04 <hjo199456@gmail.com>
2021-07-28 22:20:25 +0900

    Initial Commit

--- Delegate-TextField/ViewController.swift
+++ Delegate-TextField/ViewController.swift
@@ -50,8 +50,39 @@ class ViewController: UIViewController, UITextFieldDelegate {
         return true // false를 리턴하면 편집되지 않는다.
     }
     
-    func textFieldDidBeginEditing(_ textField: UITextField) {
+    // 텍스트 필드의 편집이 시작된 후 호출
+    func textFieldDidBeginEnditing(_ textField: UITextField) {
         print("텍스트 필드의 편집이 시작되었습니다.")
+    }
+    
+    // 텍스트 필드의 내용이 삭제될 때 호출
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 내용이 삭제됩니다")
+        return true // false를 리턴하면 삭제되지 않는다.
+    }
+    
+    // 텍스트 필드의 내용이 변경될 때 호출
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("텍스트 필드의 내용이 \(string)으로 변경됩니다")
+        return true // false를 리턴하면 내용이 변경되지 않는다.
+    }
+    
+    // 텍스트 필드의 리턴키가 눌러졌을 때 호출
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        print("텍스트 필드의 리턴키가 눌러졌습니다")
+        return true
+    }
+    
+    // 텍스트 필드 편집이 종료될 때 호출
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 편집이 종료됩니다.")
+        return true // false를 리턴하면 편집이 종료되지 않는다.
+    }
+    
+    // 텍스트 필드의 편집이 종료되었을 때 호출
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("텍스트 필드의 편집이 종료되었습니다.")
     }
     
     
