fed5bcc9724e1fbddfe7fd3dc0087403c1f5bb4e
JohnHeo04 <hjo199456@gmail.com>
2021-08-09 20:19:37 +0900

    Learned UITableViewDataSource Protocol - numberOfRowsInSection - cellForRowAt

--- MyMovieChart/ListViewController.swift
+++ MyMovieChart/ListViewController.swift
@@ -37,33 +37,10 @@ class ListViewController: UITableViewController {
     override func viewDidLoad() {
         // 아래 전부 삭제 처리
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
 }
-//        // 첫번째 행
-//        var mvo = MovieVO()
-//        mvo.title = "다크나이트"
-//        mvo.description = "영웅물에 철학에 음악까지 더해져 예술이 되다."
-//        mvo.opendate = "2008-09-04"
-//        mvo.rating = 8.95
-//        // 배열에 추가
-//        self.list.append(mvo)
-//
-//        // 두번째 행
-//        mvo = MovieVO()
-//        mvo.title = "호우시절"
-//        mvo.description = "때를 알고 내리는 좋은 비"
-//        mvo.opendate = "2009-10-08"
-//        mvo.rating = 7.31
-//        // 배열에 추가
-//        self.list.append(mvo)
-//
-//        // 세번째 행
-//        mvo = MovieVO()
-//        mvo.title = "말할 수 없는 비밀"
-//        mvo.description = "여기서 너까지 다섯 걸음"
-//        mvo.opendate = "2015-05-07"
-//        mvo.rating = 9.19
-//        // 배열에 추가
-//        self.list.append(mvo)
-//
-//    }
+
 
