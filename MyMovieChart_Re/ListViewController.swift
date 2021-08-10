d5f485bddd75c994e6d461ce6ed34f83e5022a47
JohnHeo04 <hjo199456@gmail.com>
2021-08-10 17:51:06 +0900

    전에 만들었던 실습에서 났던 오류는 온전히 나로 인한 실수였음을 인지

--- MyMovieChart_Re/ListViewController.swift
+++ MyMovieChart_Re/ListViewController.swift
@@ -13,7 +13,7 @@ class ListViewController: UITableViewController {
     var dataset = [
         ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
         ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19)
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19),
     ]
     
     // 테이블 뷰를 구성할 리스트 데이터
@@ -35,10 +35,14 @@ class ListViewController: UITableViewController {
     override func viewDidLoad() {
         }
     
+    //MARK: - 생성할 목록의 길이를 반환
+    // numberOfRowsInSection
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.list.count
     }
     
+    //MARK: - 셀 객체를 생성하여 콘텐츠를 구성한 다음 반환
+    // cellForRowAt
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 주어진 행에 맞는 데이터 소스를 읽어온다.
         let row = self.list[indexPath.row]
@@ -46,9 +50,16 @@ class ListViewController: UITableViewController {
         // 테이블 셀 객체를 직접 생성하는 대신 큐로부터 가져옴
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
         cell.textLabel?.text = row.title
+        
+        // 1, 추가사항 : 서브타이틀에 데이터 연결
+        // 보조 설명을 덧붙이기 위한 용도
+        cell.detailTextLabel?.text = row.description
+        
         return cell
     }
     
+    //MARK: - 사용자가 셀을 선택했을 때 실행할 액션을 정의
+    // didSelectRowAt
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         NSLog("선택된 행은 \(indexPath.row) 번째 행입니다")
     }
