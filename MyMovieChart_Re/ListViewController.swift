718bec50506e7a4a3acf2196885c0f0a3eb8d555
JohnHeo04 <hjo199456@gmail.com>
2021-08-13 16:13:15 +0900

    + Added UIImage Caching, contentsOfFile, named

--- MyMovieChart_Re/ListViewController.swift
+++ MyMovieChart_Re/ListViewController.swift
@@ -11,20 +11,21 @@ class ListViewController: UITableViewController {
     //MARK: - Tuple Data type
     // 튜플 아이템으로 구성된 데이터 세트
     var dataset = [
-        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95),
-        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31),
-        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19),
+        ("다크 나이트", "영웅물에 철학에 음악까지 더해져 예술이 되다.", "2008-09-04", 8.95, "darknight.jpg"),
+        ("호우시절", "때를 알고 내리는 좋은 비", "2009-10-08", 7.31, "rain.jpg"),
+        ("말할 수 없는 비밀", "여기서 너까지 다섯 걸음", "2015-05-07", 9.19, "secret.jpg"),
     ]
     
     // 테이블 뷰를 구성할 리스트 데이터
     lazy var list: [MovieVO] = {
         var datalist = [MovieVO]()
-        for (title, desc, opendate, rating) in self.dataset {
+        for (title, desc, opendate, rating, thumbnail) in self.dataset {
             let mvo = MovieVO()
             mvo.title = title
             mvo.description = desc
             mvo.opendate = opendate
             mvo.rating = rating
+            mvo.thumbnail = thumbnail
             
             datalist.append(mvo)
             
@@ -57,6 +58,10 @@ class ListViewController: UITableViewController {
         cell.opendate?.text = row.opendate
         cell.rating?.text = "\(row.rating!)"
         // ============== 여기까지 내용 변경됨 ==============
+        
+        // ============== 추가된 부분: 이미지 뷰 처리
+        cell.thumbnail.image = UIImage(named: row.thumbnail!)
+        
         
         // 구성될 셀을 반환함
         return cell
--- MyMovieChart_Re/MovieCell.swift
+++ MyMovieChart_Re/MovieCell.swift
@@ -13,5 +13,6 @@ class MovieCell: UITableViewCell {
     @IBOutlet var opendate: UILabel! // 영화 설명
     @IBOutlet var rating: UILabel! // 개봉일
     @IBOutlet var desc: UILabel! // 평점
+    @IBOutlet var thumbnail: UIImageView! // 섬네일 이미지
     
 }
